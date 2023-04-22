7a4ac5ad02fdedce17c64452ec8e84efa86e6790
Vindeshwar <vpkori@gmail.com>
2023-04-22 15:20:06 +0530

    add

--- DataTypesConcept.java
+++ DataTypesConcept.java
@@ -3,11 +3,19 @@ public class DataTypesConcept {
 
 	public static void main(String args[]) {
 		// TODO Auto-generated method stub
-	for(char c='A';c<='Z';c++)
+	/*for(char c='A';c<='Z';c++)
 	{
 		System.out.println(c +" : "+ (byte)c);
-	}
+	}*/
+	calls();			
 	
-	}
+
+}
+
+public static void calls() {
+	int a[] = new int[5];
+	a[0]=1;
+	System.out.println(a[0]);
+}
 
 }
